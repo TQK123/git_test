5df48f518a834c7fd0925d4056eec0dc8f4643dc
tqkgit <2114633320@qq.com>
2024-05-09 22:13:39 +0800

    first commit

---
+++ TestObjectPool.cpp
@@ -0,0 +1,52 @@
+#include"ObjectPool.h"
+
+//int main()
+//{
+//	//void* a = nullptr;
+//	int* a = nullptr;
+//
+//	a += 20;
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main() {
+//    int i, j;
+//    const int n = 80;
+//    double theta;
+//    char rose[n][2 * n + 1];
+//
+//    for (i = 0; i < n; i++) {
+//        for (j = 0; j < 2 * n + 1; j++) {
+//            rose[i][j] = ' ';
+//        }
+//    }
+//
+//    for (theta = 0; theta < 2 * 3.14159; theta += 0.02) {
+//        double x = 5 * sin(6 * theta) * cos(theta) + n;
+//        double y = 5 * sin(6 * theta) * sin(theta) + n;
+//
+//        if (x >= 0 && x < (2 * n) && y >= 0 && y < n) {
+//            rose[(int)y][(int)x] = '*';
+//        }
+//    }
+//
+//    for (i = 0; i < n; i++) {
+//        for (j = 0; j < 2 * n + 1; j++) {
+//            printf("%c", rose[i][j]);
+//        }
+//        printf("\n");
+//    }
+//
+//    return 0;
+//}
+
+/*int main()
+{
+	TestObjectPool();
+
+	return 0;
+}*/
